2efb246808d8b0cdf4e6f9e7296d5d43dc582693
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-03 07:05:09 -0800

    Correct typo

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/dahsboard.module.css'
+import styles from '../styles/dashboard.module.css'
 
 export default function Home() {
     return (
