956887585a0e15d88332e34a390479a7de962a3d
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-03 10:56:37 -0800

    Refactor dashboard page to use modular components

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,36 +1,20 @@
 import Head from 'next/head'
+import Footer from '../components/Footer'
+import DashboardHeader from '../components/DashboardHeader'
 import styles from '../styles/dashboard.module.css'
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Create Next App</title>
+                <title>Pesto Dolphins Dashboard</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
-                <div className={`${styles.card} blur`}>
-                    <h1 className={styles.title}>
-                        Welcome to{' '}
-                        <a
-                            href="https://github.com/brentshierk/pesto-dolphins"
-                            target="_blank"
-                            rel="noopener noreferrer">
-                                Pesto Dolphins
-                        </a>
-                    </h1>
-                </div>
+                <DashboardHeader />
             </main>
-            <footer>
-                <a
-                    href="https://github.com/brentshierk/pesto-dolphins"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by Pesto Dolphins
-                </a>
-            </footer>
+            <Footer />
       </div>
     )
 }
