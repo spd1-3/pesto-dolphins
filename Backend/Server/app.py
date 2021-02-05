c7bbfa2b211509baa31560433d05afd72891a7f5
David <ddavado@gmail.com>
2021-02-04 20:21:29 -0500

    added api endpoints, not yet returning data

--- Backend/Server/app.py
+++ Backend/Server/app.py
@@ -2,21 +2,35 @@ from flask import Flask, Response
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+MONGO_URI = os.environ.get("MONGO_URI")
 
 #dev
 import json
 
 app = Flask(__name__)
 CORS(app)
-app.config['SECRET_KEY'] = 'secret!'
-# mongoURI = "mongodb://localhost:27017/redditTickers"
-mongoURI = "mongodb+srv://pestodolphins:makeschool@cluster0.pce5s.mongodb.net/pestodolphins?retryWrites=true&w=majority"
-app.config["MONGO_URI"] =  mongoURI
+app.config["MONGO_URI"] =  MONGO_URI
 mongo = PyMongo(app)
 
-@app.route('/')
-def home():
-    return 'home :)'
+# should returns all teams
+@app.route('/teams')
+def get_teams():
+    pass
+
+# /{team_id} returns stats for referenced team
+@app.route('/team/<team_id>')
+def get_team(team_id=None):
+    pass
+
+# /{user_id} returns user information
+@app.route('/user/<user_id>')
+def get_user(user_id=None):
+    pass
 
 if __name__ == '__main__':
     app.run()
