e547cdb32b8bab35ad41978585f1bcba16c76392
Edison Li <edison4354@gmail.com>
2021-02-08 11:12:39 -0800

    Created the total message component

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Footer from '../components/Footer'
 import DashboardHeader from '../components/DashboardHeader'
 import DashboardLeaderboard from '../components/DashboardLeaderboard'
+import TotalMessages from '../components/TotalMessages'
 import styles from '../styles/dashboard.module.css'
 
 export default function Home() {
@@ -17,9 +18,10 @@ export default function Home() {
             <div className={styles.components}>
                 <DashboardLeaderboard />
                 {/* Placing this duplicate here until other components are added in order to display consistent layout */}
+                <TotalMessages />
                 <DashboardLeaderboard />
             </div>
             <Footer />
-      </div>
+        </div>
     )
 }
