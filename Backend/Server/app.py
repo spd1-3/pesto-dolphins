7e0ed0a33d6f422a3081213418604fcee9e8922d
David <ddavado@gmail.com>
2021-02-08 14:53:20 -0500

    updated pipfile, first db connection

--- Backend/Server/app.py
+++ Backend/Server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response
 from flask_cors import CORS
+from flask_mongoengine import MongoEngine
 from flask_pymongo import PyMongo
 
 import os
@@ -14,8 +15,29 @@ import json
 
 app = Flask(__name__)
 CORS(app)
+app.config["MONGODB_HOST"] = MONGO_URI
 app.config["MONGO_URI"] =  MONGO_URI
+me_db = MongoEngine()
+me_db.init_app(app)
 mongo = PyMongo(app)
+db = mongo.db.pesto
+teamsdb = mongo.db.teams
+usersdb = mongo.db.users
+channelsdb = mongo.db.channels
+
+#test db
+@app.route('/')
+def home():
+    test = db.find_one({
+        "test": "test"
+    })
+    print(test)
+    return 'hello'
+
+# should return data from a channel
+@app.route('/channel/<channel_id>')
+def get_channel(channel_id=None):
+    pass
 
 # should returns all teams
 @app.route('/teams')
