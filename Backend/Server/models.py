b2f4c5ea84ef9e4ed6f92df221bf8fe30ddb3dc6
David Cao <davidcao@Kamelines-MBP.fios-router.home>
2021-02-15 13:59:46 -0500

    added Message and Channel model

--- Backend/Server/models.py
+++ Backend/Server/models.py
@@ -7,4 +7,15 @@ class User(db.Document):
     team_name = db.StringField()
 
 class Team(db.Document):
+    team_id = db.StringField()
     name = db.StringField()
+
+class Message(db.Document):
+    message_id = db.StringField()
+    text = db.StringField()
+    sender_id = db.StringField()
+
+class Channel(db.Document):
+    channel_id = db.StringField()
+    messages = db.ListField()
+    team_id = db.StringField()
