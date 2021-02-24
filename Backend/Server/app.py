c46fa3db18d5fdc2fe61b45303ab922fd987c93c
David Cao <davidcao@Davids-MBP.fios-router.home>
2021-02-24 12:02:18 -0500

    added route to get all channel ids

--- Backend/Server/app.py
+++ Backend/Server/app.py
@@ -31,6 +31,15 @@ channelsdb = mongo.db.channels
 def home():
     return 'hello'
 
+# return all channel ids
+@app.route('/channels')
+def find_all_channels():
+    channel_ids = []
+    channels = channelsdb.find()
+    for channel in channels:
+        channel_ids.append(channel['channel_id'])
+    return json.dumps(channel_ids, default=str)
+
 # should return data from a channel or create channel
 @app.route('/channel/<channel_id>', methods=['GET', 'POST'])
 def channel_route(channel_id=None):
@@ -57,6 +66,7 @@ def messages_route(channel_id):
 def message_route(channel_id, message_id):
     channel = channelsdb.find_one({"channel_id": channel_id})
     messages = channel.get('messages', {})
+    sender_id = request.args['sender_id']
     
     if request.method == "GET":
         message = messages.get(message_id)
@@ -65,12 +75,19 @@ def message_route(channel_id, message_id):
     if request.method == "POST":
         new_message = make_message(
             text = request.args['text'],
-            sender_id = request.args['sender_id']
+            sender_id = sender_id
         )
         channelsdb.update(
             {"channel_id": channel_id},
             {"$set": {f"messages.{message_id}": new_message}}
         )
+        sender = usersdb.find_one({"user_id": sender_id})
+        if sender is not None:
+            sender_total_messages = sender.get('total_messages', 0)
+            usersdb.update(
+                {"user_id": sender_id},
+                {"total_message": sender_total_messages + 1}
+            )
         return Response(status=200)
 
 # returns user information
