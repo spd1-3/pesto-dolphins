0d51597ca6952d9624e69c1a1a3b1452d039d8eb
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-24 10:12:59 -0800

    Landing page styling

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -8,30 +8,30 @@ export default function Home() {
                 <title>Pesto Dolphins</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header className="w-full px-3 antialiased bg-indigo-600 select-none lg:px-6">
+            <header className="w-full px-3 antialiased from-tertiary to-primary bg-gradient-to-br select-none lg:px-6">
                 <div className="mx-auto max-w-7xl">
                     <nav className="flex items-center w-full h-24">
                         <div className="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
                             <a href="#_" className="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
-                                <span className="text-xl font-black leading-none text-white select-none logo">pesto<br />dolphins<span className="text-pink-500">.</span></span>
+                                <span className="text-xl font-black leading-none text-white select-none logo">pesto<br />dolphins<span className="text-primary">.</span></span>
                             </a>
-                            <div className="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-gray-900 bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex" >
-                                <div className="flex-col w-full h-auto h-full overflow-hidden bg-white rounded-lg select-none md:bg-transparent md:rounded-none md:relative md:flex md:flex-row md:overflow-auto">
-                                    <div className="flex flex-col items-center justify-center w-full h-full mt-12 text-center text-indigo-700 md:text-indigo-200 md:w-2/3 md:mt-0 md:flex-row md:items-center">
-                                        <a href="#" className="inline-block px-4 py-2 mx-2 font-medium text-left text-indigo-700 md:text-white md:px-0 lg:mx-3 md:text-center">Home</a>
-                                        <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-indigo-800 md:hover:text-white lg:mx-3 md:text-center">Features</a>
+                            <div className="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-transparent bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex" >
+                                <div className="flex-col w-full h-auto h-full overflow-hidden bg-transparent rounded-lg select-none md:bg-transparent md:rounded-none md:relative md:flex md:flex-row md:overflow-auto">
+                                    <div className="flex flex-col items-center justify-center w-full h-full mt-12 text-center text-white md:text-white md:w-2/3 md:mt-0 md:flex-row md:items-center">
+                                        <a href="#" className="inline-block px-4 py-2 mx-2 font-medium text-left text-white md:text-white md:px-0 lg:mx-3 md:text-center">Home</a>
+                                        <a href="#" className="inline-block px-0 px-4 py-2 mx-2 font-medium text-left md:px-0 hover:text-white md:hover:text-white lg:mx-3 md:text-center">Features</a>
                                     </div>
                                     <div className="flex flex-col items-center justify-end w-full h-full pt-4 md:w-1/3 md:flex-row md:py-0">
-                                        <a href="#_" className="w-full pl-6 mr-0 text-indigo-200 hover:text-white md:pl-0 md:mr-3 lg:mr-5 md:w-auto">Sign In</a>
-                                        <a href="#_" className="inline-flex items-center justify-center px-4 py-2 mr-1 text-base font-medium leading-6 text-indigo-600 whitespace-no-wrap transition duration-150 ease-in-out bg-indigo-100 border border-transparent rounded-full hover:bg-white focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700">Sign Up</a>
+                                        <a href="#_" className="w-full pl-6 mr-0 text-white hover:text-white md:pl-0 md:mr-3 lg:mr-5 md:w-auto">Sign In</a>
+                                        <a href="#_" className="inline-flex items-center justify-center px-4 py-2 mr-1 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out bg-transparent border border-transparent rounded hover:bg-transparent focus:outline-none focus:border-white focus:shadow-outline-white active:bg-transparent">Sign Up</a>
                                     </div>
                                 </div>
                             </div>
-                            <div className="absolute right-0 z-50 flex flex-col items-end w-10 h-10 p-2 mr-4 rounded-full cursor-pointer md:hidden hover:bg-indigo-700 text-indigo-200">
-                                <svg className="w-6 h-6" x-show="!showMenu" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                            <div className="absolute right-0 z-50 flex flex-col items-end w-10 h-10 p-2 mr-4 rounded-full cursor-pointer md:hidden hover:bg-white text-white">
+                                <svg className="w-6 h-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                                     <path d="M4 6h16M4 12h16M4 18h16"></path>
                                 </svg>
-                                <svg className="w-6 h-6" x-show="showMenu" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" styles={{display: "none;"}}>
+                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" styles={{display: "none;"}}>
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
                                 </svg>
                             </div>
@@ -40,7 +40,7 @@ export default function Home() {
                     <div className="container py-32 mx-auto text-center sm:px-4">
 
                         <h1 className="text-4xl font-extrabold leading-10 tracking-tight text-white sm:text-5xl sm:leading-none md:text-6xl xl:text-7xl"><span className="block">Increase engagement</span> <span className="relative inline-block mt-3 text-transparent text-white">and recognition</span></h1>
-                        <div className="max-w-lg mx-auto mt-6 text-sm text-center text-indigo-200 md:mt-12 sm:text-base md:max-w-xl md:text-lg xl:text-xl">Enhance engagement, bring in "thank you" culture and <br />boost company outcomes</div>
+                        <div className="max-w-lg mx-auto mt-6 text-sm text-center text-white md:mt-12 sm:text-base md:max-w-xl md:text-lg xl:text-xl">Enhance engagement, bring in "thank you" culture and <br />boost company outcomes</div>
                         <div className="relative flex items-center max-w-md mx-auto mt-12 text-center">
                             <input type="text" name="email" placeholder="Email Address" className="w-full h-12 px-6 py-2 font-medium text-indigo-800 rounded focus:outline-none" />
                             <span className="relative top-0 right-0 block rounded-full shadow-sm">
@@ -49,7 +49,7 @@ export default function Home() {
                                 </button>
                             </span>
                         </div>
-                        <div className="mt-8 text-sm text-indigo-300">By signing up, you agree to our terms and services.</div>
+                        <div className="mt-8 text-sm text-white">By signing up, you agree to our terms and services.</div>
                     </div>
                 </div>
             </header>
@@ -60,7 +60,7 @@ export default function Home() {
                     <p className="mt-2 text-lg text-center text-gray-600">Check out our list of awesome features below.</p>
                     <div className="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 sm:px-8 xl:px-0">
 
-                        <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden sm:rounded-xl">
+                        <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden sm:rounded-xl from-secondary-light to-tertiary-light bg-gradient-to-br border border-tertiary">
                             <div className="p-3 text-white">
                                 <svg width="75" height="75" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <circle cx="50" cy="50" r="50" fill="url(#paint0_linear)" fillOpacity="0.1"/>
@@ -82,7 +82,7 @@ export default function Home() {
                             <p className="text-base text-center text-gray-500">See live updates on Slack collaboration through a leaderboard</p>
                         </div>
 
-                        <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 sm:rounded-xl">
+                        <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 sm:rounded-xl from-secondary-light to-tertiary-light bg-gradient-to-br border border-tertiary">
                             <div className="p-3 text-white">
                                 <svg width="75" height="75" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <circle cx="50" cy="50" r="50" fill="url(#paint0_linear)" fillOpacity="0.1"/>
@@ -107,7 +107,7 @@ export default function Home() {
                             <p className="text-base text-center text-gray-500">Analyze the number messsages and reactions in a given timeframe</p>
                         </div>
 
-                        <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 sm:rounded-xl">
+                        <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 sm:rounded-xl from-secondary-light to-tertiary-light bg-gradient-to-br border border-tertiary">
                             <div className="p-3 text-white">
                             <svg width="75" height="75" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <circle cx="50" cy="50" r="50" fill="url(#paint0_linear)" fillOpacity="0.1"/>
