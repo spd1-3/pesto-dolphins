c6ebe02d2a1c02cd6df1c628deedf120c1594956
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-24 10:16:23 -0800

    Only load tailwind css for landing page

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/styles.css'
-import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Footer from '../components/Footer'
+import "tailwindcss/tailwind.css"
 
 export default function Home() {
     return (
